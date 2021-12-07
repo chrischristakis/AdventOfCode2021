0cc7d5025aea55aec397465727c912bbb5a96e4f
chrischristakis <chriscchristakis@gmail.com>
2021-12-06 22:27:31 -0500

    Optimized day 6

--- day6/day6.py
+++ day6/day6.py
@@ -1,29 +1,17 @@
-import functools
-
-
 def part1_and_part2(days):
     with open("input") as file:
-        data = [int(x) - 1 for x in file.readline().rstrip().split(",")]
         populace = [0] * 9  # Most days away from breeding a fish will be is 9 (7 days + 2 for newborns)
-
-        # Initial conditions
-        for i in data:
-            populace[i] += 1
-
-        def shift_arr(l: list, end: int):
-            temp = l[0]
-            for j in range(len(l) - 1):
-                l[j] = l[j+1]
-            l[end] += temp
+        for p in [int(x) - 1 for x in file.readline().rstrip().split(",")]:
+            populace[p] += 1
 
         # First day doesnt count
         for i in range(days-1):
             to_create = populace[0]
-            shift_arr(populace, 6)
-            populace[8] = to_create
+            # List w/o first element, and add day 1 to end of list
+            populace = populace[1:] + [to_create]
+            populace[6] += to_create
 
-        population = int(functools.reduce(lambda accum, e: accum+e, populace))
-        return population
+        return sum(populace)
 
 
 if __name__ == "__main__":
