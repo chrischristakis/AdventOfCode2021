1df6cf62eebe0acb4c47eaefd22367e1a4e33a8f
chrischristakis <chriscchristakis@gmail.com>
2021-12-18 18:54:18 -0500

    Switched day 17 to use generators

--- day17/day17.py
+++ day17/day17.py
@@ -1,25 +1,32 @@
 def part1_and_part2():
     # This problem also assumes that the target is always x>0 and y<0, makes it easier.
-    # No need to parse input, its too small. Lets just hardcode. target area: x=201..230, y=-99..-65
     x1 = 201; x2 = 230; y1 = -65; y2 = -99
+
+    def get_x(vx, x=0):
+        while x <= x2:
+            yield tuple((x >= x1, x)); x += vx; vx -= (vx > 0)
+
+    def get_y(vy, y=0):
+        while y >= y2:
+            yield tuple((y <= y1, y)); y += vy; vy -= 1
 
     # max is y2 since vy at y=0 will always be -vy0, so if vyo=y2, then when we hit y=0 our next move will hit target
     # area in the y axis. Low bound is -y2 since we can go from origin to target in one move with x = x2orx1 y = y2ory1
     maxy = 0
-    valid_velocities = set()
+    count = 0
     for vx0 in range(x2+1):
         for vy0 in range(y2, -y2):
-            y = x = 0
-            vx = vx0; vy = vy0
-            maxy_local = 0
-            while y >= y2 and x <= x2:  # We only stop when one position is out of bounds of the target area.
-                x += vx; y += vy
-                vx -= (vx>0); vy -= 1
-                maxy_local = max(y, maxy_local)
-                if x1 <= x <= x2 and y1 >= y >= y2:
-                    maxy = max(maxy_local, maxy)
-                    valid_velocities.add((vx0, vy0))
-    return maxy, len(valid_velocities)
+            y_gen = get_y(vy0)
+            x_gen = get_x(vx0)
+            local_maxy = 0
+            while (x_hit := next(x_gen, None)) is not None and (y_hit := next(y_gen, None)) is not None:
+                local_maxy = max(y_hit[1], local_maxy)
+                if x_hit[0] and y_hit[0]:
+                    maxy = max(maxy, local_maxy)
+                    count += 1
+                    break
+
+    return maxy, count
 
 
 if __name__ == "__main__":
